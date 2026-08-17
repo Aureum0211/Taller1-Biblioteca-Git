489e7677356969174dd394f9e1b8636cfd2f554a
Edward <edwardcarrillo@unicesar.edu.co>
2026-08-17 14:49:58 -0500

    feat: implementar READ de Cliente (listar y buscar)

--- src/main/java/com/mycompany/biblioteca/Cliente.java
+++ src/main/java/com/mycompany/biblioteca/Cliente.java
@@ -4,10 +4,9 @@
  */
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author lechu
- */
-public class Cliente {
-    
+public class Cliente extends Persona {
+
+    public Cliente(int id, String nombre, String telefono) {
+        super(id, nombre, telefono);
+    }
 }
--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -6,11 +6,30 @@ package com.mycompany.biblioteca;
 
 /**
  *
- * @author lechu
+ * @author Edward
  */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Aqui pondre el menu (Fase 8)
+    }
+
+    static void crearCliente() {
+        System.out.print("ID: ");
+        int id = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Telefono: ");
+        String telefono = sc.nextLine();
+
+        clientes.add(new Cliente(id, nombre, telefono));
+        System.out.println("Cliente creado correctamente.");
     }
 }
